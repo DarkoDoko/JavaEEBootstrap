85a9e25d5a3f69ca5ee9a76a064ed3d60a62daf0
Darko Doko <darko.doko1@gmail.com>
2016-08-05 18:39:01 +0200

    main example
    Implemented main example

--- MyOwnAnnotation/src/myownannotation/MyOwnAnnotation.java
+++ MyOwnAnnotation/src/myownannotation/MyOwnAnnotation.java
@@ -1,4 +1,20 @@
 package myownannotation;
 
+import java.lang.reflect.Field;
+
 public class MyOwnAnnotation {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class<?> clazz = Class.forName("myownannotation.Facade");
+        Field[] fields = clazz.getDeclaredFields();
+
+        for (Field field : fields) {
+            MyInject myInject = field.getAnnotation(MyInject.class);
+            if(myInject != null){
+                System.out.printf("Field %s is annotated with %s", field, myInject);
+            } else {
+                System.out.printf("Field %s is not annotated", field);
+            }
+        }
+    }
 }
