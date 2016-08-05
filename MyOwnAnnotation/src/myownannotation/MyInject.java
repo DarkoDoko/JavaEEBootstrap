2d2fe248cb68076466ebb577cfaea10c2ead0c9f
Darko Doko <darko.doko1@gmail.com>
2016-08-05 18:26:31 +0200

    MyInject implementation

--- MyOwnAnnotation/src/myownannotation/MyInject.java
+++ MyOwnAnnotation/src/myownannotation/MyInject.java
@@ -1,7 +1,17 @@
 package myownannotation;
 
-/**
- * Created by ddoko on 05/08/16.
- */
-public class MyInject {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MyInject {
+
+    DayTime value();
+
+    enum DayTime{
+        MORNING, EVENING
+    }
 }
--- MyOwnAnnotation/src/myownannotation/MyOwnAnnotation.java
+++ MyOwnAnnotation/src/myownannotation/MyOwnAnnotation.java
@@ -1,7 +1,4 @@
 package myownannotation;
 
-/**
- * Created by ddoko on 05/08/16.
- */
 public class MyOwnAnnotation {
 }
