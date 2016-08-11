5b90ef4c0f3ae7ba0608fcea939b1de67cccc1b9
Darko Doko <darko.doko1@gmail.com>
2016-08-11 23:51:24 +0200

    lifecycle methods
    Defined onInit and onDestroy for request scoped bean and for EJB

--- DiWithServer/src/java/com/ddoko/di/presentation/HelloService.java
+++ DiWithServer/src/java/com/ddoko/di/presentation/HelloService.java
@@ -1,12 +1,26 @@
 package com.ddoko.di.presentation;
 
 import java.util.Date;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.ejb.Stateless;
 import javax.interceptor.Interceptors;
 
 @Stateless
 @Interceptors(MethodCallLogger.class)
 public class HelloService {
+    
+    @PostConstruct
+    public void onInit() {
+        System.out.println("Creating HelloService(EJB)");
+    }
+    
+    
+    @PreDestroy
+    public void onDestroy(){
+        System.out.println("Destroying ejb");
+    }
+    
     public String sayHello(){
         return " hi there !" + new Date();
     }
--- DiWithServer/src/java/com/ddoko/di/presentation/Index.java
+++ DiWithServer/src/java/com/ddoko/di/presentation/Index.java
@@ -1,5 +1,7 @@
 package com.ddoko.di.presentation;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.enterprise.inject.Model;
 import javax.inject.Inject;
 
@@ -9,7 +11,17 @@ public class Index {
     @Inject
     HelloService hs;
     
+    @PostConstruct
+    public void onInit() {
+        System.out.println("Creating index");
+    }
+    
     public String getMessage(){
         return hs.sayHello();
+    }
+    
+    @PreDestroy
+    public void onDestroy(){
+        System.out.println("Destroying index");
     }
 }
