e7bf571d6fb4ce063bcae1912b49186d095dd784
Darko Doko <darko.doko1@gmail.com>
2016-08-15 14:39:33 +0200

    configurable timeouts

--- DiWithServer/src/java/com/ddoko/di/presentation/BigBrother.java
+++ DiWithServer/src/java/com/ddoko/di/presentation/BigBrother.java
@@ -3,11 +3,16 @@ package com.ddoko.di.presentation;
 import java.util.Date;
 import java.util.concurrent.CopyOnWriteArrayList;
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import javax.ejb.ConcurrencyManagement;
 import javax.ejb.ConcurrencyManagementType;
 import javax.ejb.Schedule;
+import javax.ejb.ScheduleExpression;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.ejb.Timeout;
+import javax.ejb.Timer;
+import javax.ejb.TimerService;
 
 /**
  *
@@ -20,16 +25,24 @@ public class BigBrother {
 
     private CopyOnWriteArrayList<String> messageQueue;
     
+    @Resource
+    TimerService ts;
+    
+    private Timer timer;
+    
     @PostConstruct
     public void initialize() {
         this.messageQueue = new CopyOnWriteArrayList<>();
+        ScheduleExpression se = new ScheduleExpression();
+        se.minute("*").hour("*").second("*/5");
+        this.timer = ts.createCalendarTimer(se);
     }
     
     public void gatherEverything(String message) {
         this.messageQueue.add(message);
     }
     
-    @Schedule(second = "*/2", minute = "*", hour = "*")
+    @Timeout
     public void batchAnalyze() {
         System.out.println("Analyzing at: " + new Date());
         
