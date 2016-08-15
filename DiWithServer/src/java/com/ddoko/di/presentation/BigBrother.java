c7aecfde4d5600df3e61c0412d6204ef5fa61cfc
Darko Doko <darko.doko1@gmail.com>
2016-08-15 14:14:05 +0200

    Asynchronous tasks in POJOs

--- DiWithServer/src/java/com/ddoko/di/presentation/BigBrother.java
+++ DiWithServer/src/java/com/ddoko/di/presentation/BigBrother.java
@@ -2,24 +2,32 @@ package com.ddoko.di.presentation;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.ejb.Asynchronous;
-import javax.ejb.Stateless;
+import javax.annotation.Resource;
+import javax.enterprise.concurrent.ManagedExecutorService;
 
 /**
  *
  * @author ddoko
  */
-@Stateless
 public class BigBrother {
     
-    @Asynchronous
-    public void gatherEverything(String message){
-        System.out.println("Save it for later: " + message);
-        
-        try{
-            Thread.sleep(1000);
-        } catch (InterruptedException ex){
-            Logger.getLogger(BigBrother.class.getName()).log(Level.SEVERE, null, ex);
-        } 
+    @Resource
+    ManagedExecutorService mes;
+
+    public void gatherEverything(String message) {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Save it for later: " + message);
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(BigBrother.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        };
+
+        mes.execute(runnable);
     }
 }
