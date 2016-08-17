e30600f91b1fc30c90f78ab8e2adc29c76064e73
Darko Doko <darko.doko1@gmail.com>
2016-08-17 22:09:49 +0200

    added JSON message

--- DiWithServer/src/java/com/ddoko/di/presentation/MessageResource.java
+++ DiWithServer/src/java/com/ddoko/di/presentation/MessageResource.java
@@ -1,7 +1,11 @@
 package com.ddoko.di.presentation;
 
+import javax.json.Json;
+import javax.json.JsonObject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  *
@@ -11,8 +15,15 @@ import javax.ws.rs.Path;
 public class MessageResource {
     
     @GET
+    @Produces(MediaType.APPLICATION_XML)
     public Message message() {
-       return new Message("hello from message"); 
+       return new Message("hello from message");
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)    
+    public JsonObject jsonMessage() {
+        return Json.createObjectBuilder().add("content", "duke42").build();
     }
     
 }
