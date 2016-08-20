8a4b8c9ac5d93a1cef4b0a4f1987da4a35210464
Darko Doko <darko.doko1@gmail.com>
2016-08-20 22:27:43 +0200

    implemented toString

--- DiWithServer/src/java/com/ddoko/di/presentation/Message.java
+++ DiWithServer/src/java/com/ddoko/di/presentation/Message.java
@@ -28,5 +28,10 @@ public class Message {
     public Message(String content) {
         this.content = content;
     }
+
+    @Override
+    public String toString() {
+        return "Message{" + "id=" + id + ", content=" + content + '}';
+    }
     
 }
